ad42ca287c7b5ddbc4a5619aca666f2b9330b5d9
rogerdpack <rogerpack2005@gmail.com>
2012-12-28 14:46:16 -0700

    it runs anyway

--- lib/copy_data_to_server.rb
+++ lib/copy_data_to_server.rb
@@ -5,12 +5,12 @@ class IncomingCopier
   def initialize local_drop_here_to_save_dir, dropbox_root_local_dir, longterm_storage_dir, sleep_time, synchro_time, 
         dropbox_size, total_client_size
     @local_drop_here_to_save_dir = File.expand_path local_drop_here_to_save_dir
-	@sleep_time = sleep_time
-	@dropbox_root_local_dir = File.expand_path dropbox_root_local_dir
-	@synchro_time = synchro_time
-	@dropbox_size = dropbox_size
-	@total_client_size = total_client_size
-	@longterm_storage_dir = longterm_storage_dir
+    @sleep_time = sleep_time
+    @dropbox_root_local_dir = File.expand_path dropbox_root_local_dir
+    @synchro_time = synchro_time
+    @dropbox_size = dropbox_size
+    @total_client_size = total_client_size
+    @longterm_storage_dir = longterm_storage_dir
     FileUtils.mkdir_p lock_dir
     FileUtils.mkdir_p dropbox_temp_transfer_dir
     FileUtils.mkdir_p track_when_client_done_dir
@@ -34,15 +34,15 @@ class IncomingCopier
   
   def sleep!(output_char, sleep_time=@sleep_time)
     sleep sleep_time
-	print output_char
+    print output_char
   end
   
   def files_incoming(use_temp_renamed_local_dir = false)
     if use_temp_renamed_local_dir
       dir = renamed_being_transferred_dir
-	else
-	  dir = @local_drop_here_to_save_dir
-	end
+    else
+      dir = @local_drop_here_to_save_dir
+    end
     Dir[dir + '/**/*']
   end
 
@@ -58,15 +58,15 @@ class IncomingCopier
   
   def wait_for_incoming_files_to_stabilize_and_rename_entire_dir
     old_size = -1
-	current_size = size_incoming_files
+    current_size = size_incoming_files
     while(current_size != old_size) 
       old_size = current_size
       sleep!('-')
-	  current_size = size_incoming_files
+      current_size = size_incoming_files
     end
-	assert !File.directory?(renamed_being_transferred_dir)
-	FileUtils.mv @local_drop_here_to_save_dir, renamed_being_transferred_dir
-	Dir.mkdir @local_drop_here_to_save_dir
+    assert !File.directory?(renamed_being_transferred_dir)
+    FileUtils.mv @local_drop_here_to_save_dir, renamed_being_transferred_dir
+    Dir.mkdir @local_drop_here_to_save_dir
   end
   
   def this_process_lock_file
@@ -84,16 +84,16 @@ class IncomingCopier
   
   def touch_the_you_can_go_for_it_file current_chunk_size
     assert have_lock?, "should be locked"
-	assert client_done_copying_files.length == 0 # just in case :P
-	assert current_transfer_ready_files.length == 0 # just in case :P
+    assert client_done_copying_files.length == 0 # just in case :P
+    assert current_transfer_ready_files.length == 0 # just in case :P
     FileUtils.touch next_you_can_go_for_it_after_size_file(current_chunk_size)
   end
   
   def wait_if_already_has_lock_files
     raise 'locking confusion detected' if File.exist? this_process_lock_file
     while Dir[lock_dir + '/*'].length > 0
-	  sleep!('l')
-	end
+      sleep!('l')
+    end
   end
   
   def create_lock_file
@@ -107,47 +107,47 @@ class IncomingCopier
   # returns true if "we got the lock"
   def wait_for_lock_files_to_stabilize
     start_time = Time.now
-    while Time.now - start_time < @synchro_time	  
-	  if !have_lock?
-	    delete_lock_file
-	    return false
-	  else
-	    sleep!('l-')
-	  end
-	end
-	true
+    while Time.now - start_time < @synchro_time      
+      if !have_lock?
+        delete_lock_file
+        return false
+      else
+        sleep!('l-')
+      end
+    end
+    true
   end
   
   def obtain_lock
-	got_it = false
-	while !got_it
-	  wait_if_already_has_lock_files
-	  create_lock_file
-	  got_it = wait_for_lock_files_to_stabilize
-	  
-	end
+    got_it = false
+    while !got_it
+      wait_if_already_has_lock_files
+      create_lock_file
+      got_it = wait_for_lock_files_to_stabilize
+      
+    end
   end
   
   def split_to_chunks
     out = []
-	current_group = []
-	current_sum = 0	
-	files_to_chunk = files_incoming(true).sort
-	raise 'no files?' if files_to_chunk.empty?
+    current_group = []
+    current_sum = 0    
+    files_to_chunk = files_incoming(true).sort
+    raise 'no files?' if files_to_chunk.empty?
     files_to_chunk.each{|f| 
-	  file_size = File.size f
-	  raise 'cannot fit that file ever [yet!]' if file_size > @dropbox_size
-	  if file_size + current_sum > @dropbox_size
-	    out << current_group
-		current_group = [f]
-		current_sum = file_size
-	  else
-	    current_group << f
-		current_sum += file_size
-	  end
-	}
-	out << current_group unless current_group.empty? # last group
-	out
+      file_size = File.size f
+      raise 'cannot fit that file ever [yet!]' if file_size > @dropbox_size
+      if file_size + current_sum > @dropbox_size
+        out << current_group
+        current_group = [f]
+        current_sum = file_size
+      else
+        current_group << f
+        current_sum += file_size
+      end
+    }
+    out << current_group unless current_group.empty? # last group
+    out
   end
   
   def renamed_being_transferred_dir
@@ -155,30 +155,30 @@ class IncomingCopier
   end
   
   def copy_chunk_to_dropbox chunk
-  	for filename in chunk
-	  relative_extra_dir = filename[(renamed_being_transferred_dir.length + 1)..-1] # like "subdir/b"
-	  possibly_new_subdir = dropbox_temp_transfer_dir + '/' + File.dirname(relative_extra_dir)
-	  FileUtils.mkdir_p possibly_new_subdir # sooo lazy, also, could we use FileUtils.cp_r here?
-	  if(File.file? filename)
-	    FileUtils.cp filename, possibly_new_subdir
-	  else
-	    assert File.directory?(filename)		
-		FileUtils.mkdir_p possibly_new_subdir + '/' + relative_extra_dir
-	  end
-	end
+      for filename in chunk
+      relative_extra_dir = filename[(renamed_being_transferred_dir.length + 1)..-1] # like "subdir/b"
+      possibly_new_subdir = dropbox_temp_transfer_dir + '/' + File.dirname(relative_extra_dir)
+      FileUtils.mkdir_p possibly_new_subdir # sooo lazy, also, could we use FileUtils.cp_r here?
+      if(File.file? filename)
+        FileUtils.cp filename, possibly_new_subdir
+      else
+        assert File.directory?(filename)        
+        FileUtils.mkdir_p possibly_new_subdir + '/' + relative_extra_dir
+      end
+    end
   end
   
   def copy_files_in_by_chunks
     for chunk in split_to_chunks
-	  size = 0
-	  chunk.each{|f| size += File.size(f) }
-	  copy_chunk_to_dropbox chunk
-  	  touch_the_you_can_go_for_it_file size
-	  wait_for_all_clients_to_copy_files_out
-	  File.delete previous_you_can_go_for_it_size_file
-	  FileUtils.rm_rf dropbox_temp_transfer_dir
-	  Dir.mkdir dropbox_temp_transfer_dir
-	end
+      size = 0
+      chunk.each{|f| size += File.size(f) }
+      copy_chunk_to_dropbox chunk
+        touch_the_you_can_go_for_it_file size
+      wait_for_all_clients_to_copy_files_out
+      File.delete previous_you_can_go_for_it_size_file
+      FileUtils.rm_rf dropbox_temp_transfer_dir
+      Dir.mkdir dropbox_temp_transfer_dir
+    end
   end
   
   def delete_lock_file
@@ -191,21 +191,21 @@ class IncomingCopier
   
   def wait_for_all_clients_to_copy_files_out
     while client_done_copying_files.length != @total_client_size
-	  sleep!('z')
-	end
-	for file in client_done_copying_files
-	  File.delete file
-	end
+      sleep!('z')
+    end
+    for file in client_done_copying_files
+      File.delete file
+    end
   end
 
   # the only one you should call...
   def go_single_transfer_out
     wait_for_any_files_to_appear
-	wait_for_incoming_files_to_stabilize_and_rename_entire_dir # them
-	obtain_lock
-	copy_files_in_by_chunks
-	delete_lock_file
-	FileUtils.rm_rf renamed_being_transferred_dir # should be safe... :)
+    wait_for_incoming_files_to_stabilize_and_rename_entire_dir # them
+    obtain_lock
+    copy_files_in_by_chunks
+    delete_lock_file
+    FileUtils.rm_rf renamed_being_transferred_dir # should be safe... :)
   end
   
   require 'copy_from_server'
--- lib/gui_setup.rb
+++ lib/gui_setup.rb
@@ -1,12 +1,13 @@
 require File.dirname(__FILE__) + "/copy_data_to_server.rb"
 $: << File.dirname(__FILE__) + "/../vendor/simple_gui_creator/lib"
 require 'simple_gui_creator'
+require 'fileutils'
 
 include SimpleGuiCreator
 extend SimpleGuiCreator
 
 @storage = Storage.new('backup_syncher')
-@storage.clear!
+
 def storage
   @storage
 end
@@ -25,30 +26,34 @@ def new_existing_dir_chooser_and_go *args
   SimpleGuiCreator.new_existing_dir_chooser_and_go *args
 end
 
+def get_input title, value
+  SimpleGuiCreator.get_input title, value
+end
+
 def re_configure
-  message = "Pick directory that is the root of your shared drive, like your_username/Google Drive or the like:"
-  show_message message
+  message = "Pick directory that is the root of your shared drive, like your_username/Google Drive or the like:"  
+  show_message message unless storage[:root_drive]
   dir = new_existing_dir_chooser_and_go message, File.expand_path('~')
   storage[:root_drive] = dir
   storage[:client_count] = get_input("how many total end storage places will there be?", 2).to_i
   storage[:shared_drive_space_to_use] = get_input("How much shared drive to use for transfers (in GB)", 2.5).to_f
   transfer_dir = File.expand_path('~/backup_synchronizer_drop_files_here')
-  File.mkdir(transfer_dir)
-  storage[:root_transfer_folder] = new_existing_dir_chooser_and_go("Pick directory where you can drop files in to have them transferred", storage[:root_transfer_folder] || transfer_dir)
+  FileUtils.mkdir_p(transfer_dir)
+  storage[:drop_into_folder] = new_existing_dir_chooser_and_go("Pick directory where you can drop files in to have them transferred", storage[:drop_into_folder] || transfer_dir)
   long_term_dir = File.expand_path('~/long_term_local_backup_copy')
-  File.mkdir(long_term_dir)
+  FileUtils.mkdir_p(long_term_dir)
   storage[:longterm_storage_local_dir] = new_existing_dir_chooser_and_go("Folder to use for long term preservation locally", storage[:longterm_storage_local_dir] || long_term_dir)
 end
 
 def setup_ui
   a.elements[:root_shared_drive].text = storage[:root_drive]
-  a.elements[:client_count].text = storage[:client_count]
-  a.elements[:shared_drive_space_to_use].text = storage[:shared_drive_space_to_use]
-  a.elements[:root_transfer_folder].text = storage[:root_transfer_folder]
+  a.elements[:client_count].text = storage[:client_count].to_s
+  a.elements[:shared_drive_space_to_use].text = storage[:shared_drive_space_to_use].to_s
+  a.elements[:drop_into_folder].text = storage[:drop_into_folder]
   a.elements[:longterm_storage_local_dir].text = storage[:longterm_storage_local_dir]
 end
 
-if !storage[:root_drive]
+if !storage[:longterm_storage_local_dir]
   re_configure
 end
 
@@ -58,10 +63,18 @@ a.elements[:re_configure].on_clicked {
   hard_exit!
 }
 
-setup_ui # init...
+a.elements[:open_drop_into_folder].on_clicked {
+  SimpleGuiCreator.show_in_explorer(storage[:drop_into_folder])
+}
 
-poll_time = 10
-synchro_time = 100 # seconds for a trivial file to propagate
+a.after_closed {
+  a.elements[:status].text = "shutting down..."
+  @subject.shut_down
+  @t1.join  
+  @t2.join
+}
+
+setup_ui # init...
 
 class Numeric
   def to_gig
@@ -69,9 +82,12 @@ class Numeric
   end
 end
 
-@subject = IncomingCopier.new a.elements[:root_transfer_folder], storage[:root_drive], storage[:longterm_storage_local_dir], poll_time, synchro_time, 
-  storage[:shared_drive_space_to_use].to_gig, total_client_size
-  
-Thread.new { loop { @subject.go_single_transfer_out } }
+poll_time = 10
+synchro_time = 100 # seconds for a trivial file to propagate
 
-loop { @subject.go_single_transfer_in }
+@subject = IncomingCopier.new storage[:drop_into_folder], storage[:root_drive], storage[:longterm_storage_local_dir], poll_time, synchro_time, 
+  storage[:shared_drive_space_to_use].to_gig, storage[:client_count]
+  
+@t1 = Thread.new { loop { @subject.go_single_transfer_out } }
+
+@t2 = Thread.new { loop { @subject.go_single_transfer_in } }
