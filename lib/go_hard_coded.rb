372f697a2b8d5f98f9d58e45e03f4ec0e34bee54
rogerdpack <rogerpack2005@gmail.com>
2012-12-28 14:22:11 -0700

    fix up hard coded

--- lib/go_hard_coded.rb
+++ lib/go_hard_coded.rb
@@ -1,3 +1,5 @@
+require File.dirname(__FILE__) + "/copy_data_to_server.rb"
+
 class Numeric
   def gig
     self * 1e9
@@ -8,7 +10,11 @@ sleep_time = 10
 synchro_time = 100 # seconds for a trivial file to propagate
 dropbox_size = 2.4.gig
 total_client_size=2
-# THESE AREN'T ACTUALLY LIVE DATA VALUES YET
 
 $subject = IncomingCopier.new 'c:\tmp\drop_stuff_in_here', 'dropbox_root_dir', 'longterm_storage_dir', sleep_time, synchro_time, 
   dropbox_size, total_client_size
+  
+puts 'remember to pre-zip them (and videos too) for now, if desired!'
+
+Thread.new { loop { $subject.go_single_transfer_out }}
+loop { $subject.go_single_transfer_in }
--- lib/gui_setup.rb
+++ lib/gui_setup.rb
@@ -2,14 +2,61 @@ $: << File.dirname(__FILE__) + "/../vendor/simple_gui_creator/lib"
 require 'simple_gui_creator'
 include SimpleGuiCreator
 
-storage = Storage.new('backup_syncher')
+@storage = Storage.new('backup_syncher')
+def storage
+  @storage
+end
 
-if !(dir = storage[:root_drive])
+@a = ParseTemplate.new.parse_setup_filename('lib\\setup.sgc')
+
+def re_configure
+  re_configure
   message = "Pick directory that is the root of your shared drive, like your_username/Google Drive or the like:"
   show_message message
   dir = new_existing_dir_chooser_and_go message, File.expand_path('~')
   storage[:root_drive] = dir
+  storage[:client_count] = get_input("how many total end storage places will there be?", 2).to_i
+  storage[:shared_drive_space_to_use] = get_input("How much shared drive to use for transfers (in GB)", 2.5).to_f
+  transfer_dir = File.expand_path('~/backup_synchronizer_drop_files_here')
+  File.mkdir(transfer_dir)
+  storage[:root_transfer_folder] = new_existing_dir_chooser_and_go("Pick directory where you can drop files in to have them transferred", storage[:root_transfer_folder] || transfer_dir)
+  long_term_dir = File.expand_path('~/long_term_local_backup_copy')
+  File.mkdir(long_term_dir)
+  storage[:longterm_storage_local_dir] = new_existing_dir_chooser_and_go("Folder to use for long term preservation locally", storage[:longterm_storage_local_dir] || long_term_dir)
 end
 
-a = ParseTemplate.new().parse_setup_filename('lib\\setup.sgc')
-a.elements[:root_shared_drive].text = storage[:root_drive]
+def setup_ui
+  a.elements[:root_shared_drive].text = storage[:root_drive]
+  a.elements[:client_count].text = storage[:client_count]
+  a.elements[:shared_drive_space_to_use].text = storage[:shared_drive_space_to_use]
+  a.elements[:root_transfer_folder].text = storage[:root_transfer_folder]
+  a.elements[:longterm_storage_local_dir].text = storage[:longterm_storage_local_dir]
+end
+
+if !storage[:root_drive]
+  re_configure
+end
+
+a.elements[:re_configure].on_clicked {
+  re_configure
+  show_message "ok, restarting app now, hope you weren't in the middle of a transfer..."
+  hard_exit!
+}
+
+setup_ui # init...
+
+poll_time = 10
+synchro_time = 100 # seconds for a trivial file to propagate
+
+class Numeric
+  def to_gig
+    self * 1e9
+  end
+end
+
+@subject = IncomingCopier.new a.elements[:root_transfer_folder], storage[:root_drive], storage[:longterm_storage_local_dir], poll_time, synchro_time, 
+  storage[:shared_drive_space_to_use].to_gig, total_client_size
+  
+Thread.new { loop { @subject.go_single_transfer_out } }
+
+loop { @subject.go_single_transfer_in }
